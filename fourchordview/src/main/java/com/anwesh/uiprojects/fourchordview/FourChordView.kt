28db5f19cc09fac5d892e2f135fdebcc89b4eacd
Anwesh43 <anweshthecool0@gmail.com>
2019-01-19 09:29:12 +0530

    adding constants and importing libraries

--- fourchordview/src/main/java/com/anwesh/uiprojects/fourchordview/FourChordView.kt
+++ fourchordview/src/main/java/com/anwesh/uiprojects/fourchordview/FourChordView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.fourchordview
 /**
  * Created by anweshmishra on 19/01/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.8f
+val foreColor : Int = Color.parseColor("#1976D2")
+val backColor : Int = Color.parseColor("#BDBDBD")
