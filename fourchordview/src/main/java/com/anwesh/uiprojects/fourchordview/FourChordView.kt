99e3ff17acf859e631ad7ba89bc454dfc9cd8ce8
Anwesh43 <anweshthecool0@gmail.com>
2019-01-19 10:24:00 +0530

    adding FCNode class

--- fourchordview/src/main/java/com/anwesh/uiprojects/fourchordview/FourChordView.kt
+++ fourchordview/src/main/java/com/anwesh/uiprojects/fourchordview/FourChordView.kt
@@ -119,4 +119,48 @@ class FourChordView(ctx : Context) : View(ctx) {
             }
         }
     }
-}
+
+    data class FCNode(var i : Int, val state : State = State()) {
+
+        private var next : FCNode? = null
+        private var prev : FCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = FCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawFCNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : FCNode {
+            var curr : FCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+    }
+ }
