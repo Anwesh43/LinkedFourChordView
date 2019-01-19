2a7fe6936631ca24d9fbf23fce06a361a0cd6dfc
Anwesh43 <anweshthecool0@gmail.com>
2019-01-19 10:10:29 +0530

    adding drawFCNode canvas extension method

--- fourchordview/src/main/java/com/anwesh/uiprojects/fourchordview/FourChordView.kt
+++ fourchordview/src/main/java/com/anwesh/uiprojects/fourchordview/FourChordView.kt
@@ -27,3 +27,30 @@ fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawFCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.color = foreColor
+    paint.style = Paint.Style.STROKE
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    save()
+    translate(w/2, gap * (i + 1))
+    rotate(90f * sc2)
+    drawCircle(0f, 0f, size, paint)
+    for (j in 0..(lines - 1)) {
+        val x : Float = (size) * Math.cos(Math.PI/lines).toFloat()
+        val y : Float = (size) * Math.sin(Math.PI/lines).toFloat()
+        val sc : Float = sc1.divideScale(j, lines)
+        save()
+        rotate(90f * j)
+        drawLine(x, y, x - 2 * x * sc, y, paint)
+        restore()
+    }
+    restore()
+}
