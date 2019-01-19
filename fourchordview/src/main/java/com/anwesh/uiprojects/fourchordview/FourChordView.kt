f1c6aed154365640bb100515a3daaa6587a96e87
Anwesh43 <anweshthecool0@gmail.com>
2019-01-19 10:29:50 +0530

    using renderer in FourChordView class

--- fourchordview/src/main/java/com/anwesh/uiprojects/fourchordview/FourChordView.kt
+++ fourchordview/src/main/java/com/anwesh/uiprojects/fourchordview/FourChordView.kt
@@ -58,15 +58,16 @@ fun Canvas.drawFCNode(i : Int, scale : Float, paint : Paint) {
 class FourChordView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val renderer : Renderer = Renderer(this)
 
     override fun onDraw(canvas : Canvas) {
-
+        renderer.render(canvas, paint)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
@@ -203,7 +204,7 @@ class FourChordView(ctx : Context) : View(ctx) {
             }
         }
 
-        fun handleTap(cb : () -> Unit) {
+        fun handleTap() {
             fc.startUpdating {
                 animator.start()
             }
